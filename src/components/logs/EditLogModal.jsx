56362c88222fdb8ff701e4e43e13c7e96fb4f879
Olga <cheraneva197@gmail.com>
2020-03-27 18:59:31 +0500

    Created an edit log modal

---
+++ src/components/logs/EditLogModal.jsx
@@ -0,0 +1,79 @@
+import React, {useState} from 'react';
+import M from 'materialize-css/dist/js/materialize.min.js';
+
+export const EditLogModal = () => {
+    const [message, setMessage] = useState('');
+    const [tech, setTech] = useState('');
+    const [attention, setAttention] = useState(false);
+
+    const onSubmit = (e) => {
+        if (message === '' || tech === '') {
+            M.toast({html: 'Please enter a message and tech'});
+        } else {
+            console.log(message, tech, attention);
+            setMessage('');
+            setTech('');
+            setAttention(false);
+        }
+    };
+
+    return (
+        <div id="edit-log-modal" className="modal modal-size-75">
+            <div className="modal-content">
+                <h4>Enter System Log</h4>
+                <div className="row">
+                    <div className="input-field">
+                        <input
+                            type="text"
+                            name="message"
+                            value={message}
+                            onChange={(e) => setMessage(e.target.value)}
+                        />
+                        <label htmlFor="message" className="active">
+                            Log Message
+                        </label>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="input-field">
+                        <select
+                            name="tech"
+                            value={tech}
+                            className="browser-default"
+                            onChange={(e) => setTech(e.target.value)}
+                        >
+                            <option value="" disabled>
+                                Select Technician
+                            </option>
+                        </select>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="input-field">
+                        <p>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    className="filled-in"
+                                    checked={attention}
+                                    value={attention}
+                                    onChange={() => setAttention(!attention)}
+                                />
+                                <span>Needs Attention</span>
+                            </label>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="modal-footer">
+                <a
+                    href="#!"
+                    className="modal-close waves-effect waves-light blue btn"
+                    onClick={onSubmit}
+                >
+                    Enter
+                </a>
+            </div>
+        </div>
+    );
+};
